84c7290da18d0b79e06da49cf72680e1973e11e5
danielcristho <pepuhodaniel93.learn@gmail.com>
2022-09-07 00:22:34 +0700

    feat: make migrations

--- menu/models.py
+++ menu/models.py
@@ -1,3 +1,14 @@
+from unicodedata import name
 from django.db import models
 
 # Create your models here.
+
+class Menu(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.IntegerField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now_add=True)
+    
+    def __str(self):
+        return self.name
